2268b68013fa2168261c18a45cd4cf2166f23884
Xiaojian Deng <xjd001@gmail.com>
2016-03-01 19:37:03 -0800

    Implemented k-means clustering for 2 features

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -64,7 +64,9 @@ plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
+from sklearn.cluster import KMeans
+reg = KMeans(n_clusters = 2)
+pred = reg.fit_predict(finance_features,poi)
 
 
 
